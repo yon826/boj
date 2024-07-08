2abeec8664194578c708784c89ea78d22386c422
yon826 <127848152+yon826@users.noreply.github.com>
2024-07-08 11:54:59 +0900

    [Gold V] Title: 괄호의 값, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+bracket = list(input())
+ans = 0
+tmp =1
+stack = []
+
+for i in range(len(bracket)):
+    if bracket[i] == '(':
+        stack.append(bracket[i])
+        tmp*=2
+
+    elif bracket[i] == '[':
+        stack.append(bracket[i])
+        tmp*=3
+
+    elif bracket[i] == ')':
+        if not stack or stack[-1]!='(':
+            ans = 0
+            break
+        if bracket[i-1] =='(':
+            ans+=tmp
+        stack.pop()
+        tmp//=2
+    elif bracket[i] == ']':
+        if not stack or stack[-1]!='[':
+            ans = 0
+            break
+        if bracket[i-1] =='[':
+            ans+=tmp
+        stack.pop()
+        tmp//=3
+if stack:
+    print(0)
+else:
+    print(ans)
