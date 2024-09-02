2fef9e8784c1d99c74bea2f32cab039c7860b707
yon826 <127848152+yon826@users.noreply.github.com>
2024-09-02 22:28:23 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 1660 ms, Memory: 125336 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(N), N-2)
