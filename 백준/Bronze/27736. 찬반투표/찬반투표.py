03715b62c68682fc4362523f51598bdd990e8149
yon826 <127848152+yon826@users.noreply.github.com>
2024-09-12 14:29:36 +0900

    [Bronze III] Title: 찬반투표, Time: 32 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27736. 찬반투표/찬반투표.py
+++ 백준/Bronze/27736. 찬반투표/찬반투표.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
-if N == 1:
-    if A[0] == 1:
-        print("APPROVED")
-    elif A[0] == -1:
-        print("REJECTED")
-    else:
+if N%2 == 1:
+    if A.count(0) >= N/2:
         print("INVALID")
+    elif A.count(1) > A.count(-1):
+        print("APPROVED")
+    else:
+        print("REJECTED")
 else:
     if A.count(0) >= N//2:
         print("INVALID")
