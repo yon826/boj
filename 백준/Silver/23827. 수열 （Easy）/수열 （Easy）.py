55f8a2c4751eb93557084e042a2d94c9e1c67b1d
yon826 <127848152+yon826@users.noreply.github.com>
2024-09-14 00:23:14 +0900

    [Silver IV] Title: 수열 (Easy), Time: 420 ms, Memory: 86564 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+prefix = [0]*n
+
+for i in range(n-1, 0, -1):
+    prefix[n-i] = prefix[n-i-1] + A[i]
+
+sum = 0
+for i in range(n-1):
+    sum += A[i] * prefix[len(prefix)-1-i]
+print(sum%1000000007)
