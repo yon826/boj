aa7719d7b7c727728ab786e998b39c0ddf4fc811
yon826 <127848152+yon826@users.noreply.github.com>
2024-09-19 14:25:13 +0900

    [Bronze III] Title: N M 찍기, Time: 560 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n , m = map(int, input().split())
+count = 1
+for i in range(n):
+    for j in range(m-1):
+        print(count, end = ' ')
+        count += 1
+    print(count)
+    count += 1
